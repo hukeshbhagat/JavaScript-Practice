bcf997a39d0d40fb543d83dfb02a2cdedcfb5370
hukeshbhagat <hukeshbhagat5@gmail.com>
2026-09-10 17:43:05 +0530

    Day 2 Callback Practice

--- 04.Callbacks-Promise/script.js
+++ 04.Callbacks-Promise/script.js
@@ -61,7 +61,87 @@
 
 // part 4
 
-let user = ["Lucky", "Rahul", "Aman"];
+// let user = ["Lucky", "Rahul", "Aman"];
+
+// function processUser(users, callback) {
+//   for (let i of users) {
+//     console.log(callback(i));
+//   }
+// }
+
+// function welcomeUser(name) {
+//   return `welcome ${name}`;
+// }
+
+// processUser(user, welcomeUser);
+
+// function showlength(name) {
+//   return `${name} = ${name.length}`;
+// }
+
+// processUser(user, showlength);
+
+// Day 2
+
+// function greet(name) {
+//   console.log(`Hello ${name}`)
+// }
+
+// function processUser(name,callback) {
+//   callback(name)
+// }
+
+// processUser("Hukesh",greet)
+
+// function square(num) {
+//   console.log(num * num)
+// }
+
+// function processNumber(num,callback) {
+//   callback(num)
+// }
+
+// processNumber(5,square)
+
+// part 2
+
+// function calculate(a,b,callback) {
+//   return callback(a,b)
+
+// }
+
+// function add(a,b) {
+//   return a + b
+// }
+
+// function sub(a,b) {
+//   return a - b
+// }
+
+// function mul(a,b) {
+//   return a * b
+// }
+
+// console.log(calculate(5,5,add))
+// console.log(calculate(5,5,sub))
+// console.log(calculate(5,5,mul))
+
+// let numbers = [2, 4, 6, 8, 10];
+
+// function processArray(arr, callback) {
+//   for(let i of arr) {
+//     console.log(callback(i))
+//   }
+
+// }
+
+// function square(num) {
+//   return num * num
+// }
+
+// processArray(numbers,square);
+
+let users = ["Lucky", "Rahul", "Aman"];
 
 function processUser(users, callback) {
   for (let i of users) {
@@ -70,13 +150,13 @@ function processUser(users, callback) {
 }
 
 function welcomeUser(name) {
-  return `welcome ${name}`;
+  return `Welcome ${name}`;
 }
 
-processUser(user, welcomeUser);
+processUser(users, welcomeUser);
 
-function showlength(name) {
+function showLength(name) {
   return `${name} = ${name.length}`;
 }
 
-processUser(user, showlength);
+processUser(users, showLength);
