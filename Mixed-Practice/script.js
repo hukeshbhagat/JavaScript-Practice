f91bd28fe3fc6f245836c78acbfb917f17d59be0
hukeshbhagat <hukeshbhagat5@gmail.com>
2026-09-15 18:55:43 +0530

    mix practice

--- Mixed-Practice/script.js
+++ Mixed-Practice/script.js
@@ -76,3 +76,124 @@
 
 // }
 // console.log(showResult(student))
+
+
+// Day 2 
+
+// part 1 
+
+// function profile(username,password) {
+//     return new Promise((resolve,reject) => {
+//     if(username === "Hukesh" && password === 1234) {
+//         resolve("Login Successful")
+//     }else {
+//         reject("Login Failed")
+//     }
+// })
+// }
+
+
+
+
+// async function login(username,password) {
+    
+//         try{ 
+//             let result = await profile(username,password)
+//             console.log(result)
+//             console.log("Dashboard Loaded")
+//             console.log("Welcome User")
+//         } 
+//         catch(error) {
+//             console.log(error)
+
+//         }
+
+
+//     }
+//    login("Hukesh",1234)
+
+
+// part 2
+
+let arr = [12,5,20,8,15,3]
+
+// function findMax(arr) {
+//     let max = arr[0];
+//     for(let i of arr) {
+//         if(i >= max) {
+//             max = i
+//         }
+//     }
+//     return max
+// }
+// console.log(findMax(arr))
+
+//  let arr = [12,5,20,8,15,3]
+
+// function findMin(arr) {
+//     let min = arr[0];
+//     for(let i of arr) {
+//         if(i <= min) {
+//             min = i
+//         }
+//     }
+//     return min
+// }
+// console.log(findMin(arr))
+
+
+
+// function evenNo(arr) {
+//     let count = 0;
+//     for(let i of arr) {
+//         if(i % 2 === 0) {
+//             count++
+//         }
+//     }
+//     return count
+// }
+// console.log(evenNo(arr))
+
+
+// function getTotal(arr) {
+//     let sum = 0
+//     for(let i of arr) {
+//         sum += i;
+//     }
+//     return sum
+// }
+// console.log(getTotal(arr))
+
+
+// part 3
+
+// part 4
+
+let student = {
+    name : "Hukseh",
+    marks : [2,3,5],
+    city : "Nagpur"
+}
+
+function showResult(student) {
+    let {name,marks,city} = student
+
+    let mark = 0;
+
+    for(let i of marks) {
+        mark += i
+    }
+    
+
+    
+
+
+}
+
+
+
+
+
+
+        
+    
